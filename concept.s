9b820a1e4511bca737482e5ec48d9a5688cb900a
Marek Felšöci <marek.felsoci@etu.unistra.fr>
2015-11-27 21:38:09 +0100

    Fonction ChargerTampons completée

--- concept.s
+++ concept.s
@@ -1,8 +1,11 @@
 .data
 
 ouverture_fichier: .asciiz "Saisir le nom du fichier a compresser: "
-fichier_a_compresser: .space 30
+fichier_a_compresser: .space 31
 fichier_sortie: .asciiz "out.lz77"
+
+tampon_R: .space 6
+tampon_L: .space 5
 
 buffer: .space 2048
 fnf: .asciiz "NOT FOUND"
@@ -12,6 +15,9 @@ test: .asciiz "C:\\test.txt"
 .globl __start
 
 __start:
+
+li $s5 5
+li $s4 6
 
 #Demande d'entrer le nom du fichier a compresser:
 la $a0 ouverture_fichier
@@ -29,17 +35,16 @@ jal OuvrirPourLire
 jal OuvrirPourEcrire
 
 #Lecture depuis le fichier
-
+ChargerBuffer:
 move $a0 $s6
 la $a1 buffer
 li $a2 2000
 li $v0 14
 syscall
 
-la $t0 buffer
-lb $a0 5($t0)
-li $v0 11
-syscall
+li $a0 12
+
+jal ChargerTampons
 
 jal FermerTout 
 
@@ -50,7 +55,16 @@ li $v0 10
 syscall
 
 OuvrirPourLire: #Fonction d'ouverture de fichier pour la lecture
-la $a0 test #nom du fichier a ouvrir
+xor $a2, $a2, $a2 #on iniatilse a2 ? 0
+loop:
+    lbu $a3, fichier_a_compresser($a2)  
+    addiu $a2, $a2, 1
+    bnez $a3, loop       # boucle pour rechercher le caract?re NULL
+    beq $a1, $a2, skip   # si le nombre de caract?re=30 on ne fait rien
+    subiu $a2, $a2, 2    # sinon on supprime le dernier caract?re
+    sb $0, fichier_a_compresser($a2)     # et on le remplace par le caract?re NULL
+skip:
+la $a0 fichier_a_compresser #nom du fichier a ouvrir
 li $a1 0 #0: pour la lecture
 li $a2 0 #0: mode est ignore
 li $v0 13 #ouverture
@@ -80,4 +94,75 @@ li $v0 16
 syscall
 li $v0 16
 syscall
+jr $ra
+
+#Parametres -> a0: position initiale
+ChargerTampons:
+#Prologue:
+subu $sp $sp 16
+sw $s5 12($sp)
+sw $s4 8($sp)
+sw $a0 4($sp)
+sw $ra 0($sp)
+#Corps:
+move $t0 $a0
+#add $t0 $t0 1
+li $t1 0
+add $t1 $t0 $s5
+li $t3 0
+#Chrgement du tampon de lecture a partir du buffer
+ChargerTamponLecture:
+bge $t0 $t1 FinChargementTamponLecture
+lb $t2 buffer($t0)
+sb $t2 tampon_L($t3)
+addi $t0 $t0 1
+addi $t3 $t3 1
+j ChargerTamponLecture
+#Reinitialisation des compteurs et registres temporaires
+FinChargementTamponLecture:
+move $t1 $a0
+sub $t0 $t1 $s4
+li $t3 0
+#Chargement du tampon de recherche a partir du buffer
+Decalage:
+bge $t0 0 ChargerTamponRecherche
+addi $t0 $t0 1
+addi $t3 $t3 1
+j Decalage
+ChargerTamponRecherche:
+bge $t0 $t1 FinChargement
+lb $t2 buffer($t0)
+sb $t2 tampon_R($t3)
+addi $t3 $t3 1
+addi $t0 $t0 1
+j ChargerTamponRecherche
+#Fin du chargement (teste d'impression)
+FinChargement:
+li $t0 0
+li $t1 0
+#Impression du tampon de recherche
+ImpressionTamponRecherche:
+bge $t0 $s4 ImpressionTamponLecture
+lb $t2 tampon_R($t0)
+move $a0 $t2
+li $v0 11
+syscall
+addi $t0 $t0 1
+j ImpressionTamponRecherche
+#Impression du tampon de lecture
+ImpressionTamponLecture:
+bge $t1 $s5 Prologue
+lb $t2 tampon_L($t1)
+move $a0 $t2
+li $v0 11
+syscall
+addi $t1 $t1 1
+j ImpressionTamponLecture
+#Prologue:
+Prologue:
+lw $s5 12($sp)
+lw $s4 8($sp)
+lw $a0 4($sp)
+lw $ra 0($sp)
+addu $sp $sp 16
 jr $ra
