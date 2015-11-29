d87f0bb155f5d2181e38ae78604f492203a04edb
Marek Felšöci <marek.felsoci@etu.unistra.fr>
2015-11-29 23:35:18 +0100

    Ajout des fonctions auxiliaires pour la fonction de recherche de motifs dans le tampon de recherche

--- concept.s
+++ concept.s
@@ -6,6 +6,8 @@ fichier_sortie: .asciiz "out.lz77"
 
 tampon_R: .space 6
 tampon_L: .space 5
+copie_tampon_L: .space 5
+copie_tampon_R: .space 6
 
 buffer: .space 2048
 fnf: .asciiz "NOT FOUND"
@@ -42,9 +44,14 @@ li $a2 2000
 li $v0 14
 syscall
 
-li $a0 12
+li $a0 6
 
 jal ChargerTampons
+
+li $a0 1
+li $a1 3
+
+jal SousChaine
 
 jal FermerTout 
 
@@ -151,7 +158,7 @@ addi $t0 $t0 1
 j ImpressionTamponRecherche
 #Impression du tampon de lecture
 ImpressionTamponLecture:
-bge $t1 $s5 Prologue
+bge $t1 $s5 Epilogue
 lb $t2 tampon_L($t1)
 move $a0 $t2
 li $v0 11
@@ -159,10 +166,158 @@ syscall
 addi $t1 $t1 1
 j ImpressionTamponLecture
 #Prologue:
-Prologue:
+Epilogue:
 lw $s5 12($sp)
 lw $s4 8($sp)
 lw $a0 4($sp)
 lw $ra 0($sp)
 addu $sp $sp 16
+jr $ra
+
+#Comparaison de deux chaines de caracteres
+#Param: $a0 -> longueur des chaines de caracteres a comparer
+ComparerChaines:
+#Prologue:
+subu $sp $sp 8
+sw $a0 4($sp)
+sw $ra 0($sp)
+#Corps:
+li $t0 0 #initialiser le compteur a 0
+move $t1 $a0
+Comparaison:
+bge $t0 $t1 FinComparaison
+lb $t2 copie_tampon_L($t0)
+lb $t3 copie_tampon_R($t0)
+bne $t2 $t3 PasEgaux
+j Comparaison
+
+#Epilogues:
+PasEgaux:
+li $v0 0
+lw $a0 4($sp)
+lw $ra 0($sp)
+addu $sp $sp 8
+jr $ra
+
+FinComparaison:
+li $v0 1
+lw $a0 4($sp)
+lw $ra 0($sp)
+addu $sp $sp 8 
+jr $ra
+
+SousChaine:
+#Prologue:
+subu $sp $sp 12
+sw $a1 8($sp) #longueur
+sw $a0 4($sp) #position du debut
+sw $ra 0($sp)
+#Corps:
+li $t0 0 #compteur a 0
+move $t2 $a0
+Extraction:
+bge $t0 $a1 FinExtraction
+lb $t1 tampon_L($t2)
+sb $t1 copie_tampon_L($t0)
+addi $t2 $t2 1
+addi $t0 $t0 1
+j Extraction
+
+FinExtraction:
+li $t0 0
+Impression:
+bge $t0 $s4 EpilogueSousChaine
+lb $t1 copie_tampon_L($t0)
+move $a0 $t1
+li $v0 11
+syscall
+addi $t0 $t0 1
+j Impression
+
+EpilogueSousChaine:
+lw $a1 8($sp) 
+lw $a0 4($sp) 
+lw $ra 0($sp)
+addu $sp $sp 12
+
+jr $ra
+
+SousChaineRecherche:
+#Prologue:
+subu $sp $sp 12
+sw $a1 8($sp) #longueur
+sw $a0 4($sp) #position du debut
+sw $ra 0($sp)
+#Corps:
+li $t0 0 #compteur a 0
+move $t2 $a0
+ExtractionRecherche:
+bge $t0 $a1 FinExtractionRecherche
+lb $t1 tampon_R($t2)
+sb $t1 copie_tampon_R($t0)
+addi $t2 $t2 1
+addi $t0 $t0 1
+j ExtractionRecherche
+
+FinExtractionRecherche:
+li $t0 0
+ImpressionRecherche:
+bge $t0 $s5 EpilogueSousChaineRecherche
+lb $t1 copie_tampon_R($t0)
+move $a0 $t1
+li $v0 11
+syscall
+addi $t0 $t0 1
+j ImpressionRecherche
+
+EpilogueSousChaineRecherche:
+lw $a1 8($sp) 
+lw $a0 4($sp) 
+lw $ra 0($sp)
+addu $sp $sp 12
+
+jr $ra
+
+#Fonction INDEX:
+#Params: $s4 -> longueur du tampon R
+#        $a1 -> longueur du string a comparer (copie_tampon_L)
+INDEX:
+#Prologue:
+subu $sp $sp 12
+sw $s4 8($sp)
+sw $a1 4($sp)
+sw $ra 0($sp)
+#Corps:
+li $t0 0 #compteur a 0
+li $t1 0
+sub $t1 $s4 $a1
+BoucleIndex:
+bge $t0 $t1 FinBoucleIndex
+move $a0 $t0
+jal SousChaineRecherche
+move $a0 $a1
+jal ComparerChaines
+BoucleIndex2:
+beq $v0 1 FinTotale
+addi $t0 $t0 1
+j BoucleIndex
+
+FinTotale:
+blt $t0 $t1 Trouve
+j ExistePas
+
+Trouve:
+li $v0 1
+lw $s4 8($sp)
+lw $a1 4($sp)
+lw $ra 0($sp)
+addu $sp $sp 12
+jr $ra
+
+ExistePas:
+li $v0 0
+lw $s4 8($sp)
+lw $a1 4($sp)
+lw $ra 0($sp)
+addu $sp $sp 12
 jr $ra
